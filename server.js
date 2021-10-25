e18c5b8f4c7a0481bbeea7bc379da5c9b7b4dcb7
Tal Sagie <Talsagie19@gmail.com>
2021-10-25 15:55:14 +0300

    db added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const connectdb = require('./config/db');
 const app =express();
+
+
+//connect db
+connectdb();
 
 app.get('/',(req,res)=>{
     res.send('api running');
