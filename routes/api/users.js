f149e60af2df024409b8dafbc1e36626dedd9dc0
Tal Sagie <Talsagie19@gmail.com>
2021-10-28 12:51:36 +0300

    Changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,82 +1,87 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravater = require('gravatar');
-const User = require('../../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const {check ,validationResult} = require('express-validator')
-const config = require('config');
+const gravater = require("gravatar");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const config = require("config");
 
 //@route POST api/users
 //@desc Register user
 //@acsses Public
-router.post('/',[check('name','Name is requierd').not().isEmpty(),
-check('email','include a vaild email adress').isEmail(),
-check('password','please enter a password with 6 or more characters').isLength({min:6})],
-async (req,res)=>{
+router.post(
+  "/",
+  [
+    check("name", "Name is requierd").not().isEmpty(),
+    check("email", "include a vaild email adress").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const {name,email,password} = req.body;
+    const { name, email, password } = req.body;
 
-    try{
-        let user = await User.findOne({email});
+    try {
+      let user = await User.findOne({ email });
 
-        if(user){
-            return res.status(400).json({errors:[{msg:'User already exists'}]});
-             
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      const avatar = gravater.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //return jwtoken//return jwtoken//return jwtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          console.log(token);
+          res.json({ token });
         }
-        const avatar = gravater.url(email,{
-            s:'200',
-            r:'pg',
-            d:'mm'
-        })
+      );
 
+      //return jwtoken//return jwtoken//return jwtoken//return jwtoken
 
-        user=new User({
-            name,
-            email,
-            avatar,
-            password
-        });
-
-        const salt = await bcrypt.genSalt(10);
-
-        user.password = await bcrypt.hash(password,salt);
-        await user.save();
-
-        //return jwtoken//return jwtoken//return jwtoken
-        const payload = {
-            user:{
-                id:user.id
-            }
-        };
-
-        jwt.sign(payload,
-            config.get('jwtSecret'),{
-                expiresIn:3600
-            },(err,token)=>{
-                if(err) throw err;
-                console.log(token);
-                res.json({token});
-            });
-
-
-
-        //return jwtoken//return jwtoken//return jwtoken//return jwtoken
-
-
-
-        res.send('User Reg');
-    }catch(err){
-        console.error(err.message);
-        res.status(500).send('server error');
+      res.send("User Reg");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
     }
-
-
-
-});
+  }
+);
 
 module.exports = router;
